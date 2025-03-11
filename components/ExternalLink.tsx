0ba12a16df7fcda4cc97681cda67f0b4f96b491f
kellyannclark <103697379+kellyannclark@users.noreply.github.com>
2025-03-11 12:48:04 -0600

    Updated ExternalLink.tsx to use Linking.openURL instead of expo-web-browser

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,9 +1,8 @@
 import { Link } from 'expo-router';
-import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Linking } from 'react-native';
 
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
+type Props = ComponentProps<typeof Link> & { href: string };
 
 export function ExternalLink({ href, ...rest }: Props) {
   return (
@@ -13,10 +12,10 @@ export function ExternalLink({ href, ...rest }: Props) {
       href={href}
       onPress={async (event) => {
         if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
+          // Prevent the default behavior of opening in the browser on native.
           event.preventDefault();
-          // Open the link in an in-app browser.
-          await openBrowserAsync(href);
+          // Open the link using React Native's Linking API.
+          await Linking.openURL(href);
         }
       }}
     />
