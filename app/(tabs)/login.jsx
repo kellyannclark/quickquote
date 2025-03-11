fcba648c9a77045199939495c23c32e18fb995a9
Shaira <sil21019@byui.edu>
2025-03-11 21:34:50 +0000

    login btn added

--- app/(tabs)/login.jsx
+++ app/(tabs)/login.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
-import { SafeAreaView, View, Text, TextInput, StyleSheet, Image} from 'react-native'
+import { SafeAreaView, View, Text, TextInput, StyleSheet, Image, TouchableOpacity} from 'react-native'
 import quickQuoteLogo from "@/assets/images/react-logo.png"
+import { Link } from '@react-navigation/native';
 
 
 export default function Login() {
@@ -22,6 +23,9 @@ export default function Login() {
         <View style={styles.input}>
             {/* Username */}
             <TextInput 
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="email-address"
             style={styles.inputControl}
             placeholder="Username"
             placeholderTextcolor="grey"
@@ -32,6 +36,7 @@ export default function Login() {
 
              {/* Password */}
             <TextInput 
+            secureTextEntry
             style={styles.inputControl}
             placeholder="Password"
             placeholderTextcolor="grey"
@@ -39,9 +44,26 @@ export default function Login() {
             onChangeText={password => setForm({ ...form, password})}
             >
             </TextInput>
-        </View>
 
-      </View>
+            {/* Forgot Password */}
+            <Link style={styles.subtext}>Forgot Password</Link>
+            
+        </View>  {/* End - Input */}
+        
+        {/* Login Button */}
+        <View style={styles.formAction}>
+            <TouchableOpacity onPress={()=>{
+                // handle onPress
+            }}>
+
+            <View style={styles.logBtn}>
+                <Text style={styles.logBtnText}>Login</Text>
+            </View>
+            </TouchableOpacity>
+
+        </View> {/* End - View Login Button */}
+
+      </View> {/* End - View Form */}
     </SafeAreaView>
   );
 }
@@ -51,7 +73,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    marginTop: '40%',
+    marginTop: '30%',
     marginHorizontal: '20%'
   },
   headerImg:{
@@ -67,12 +89,15 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Regular',
     marginTop:'10%'
   },
+  subtext: {
+    color: 'grey',
+    fontSize: 13,
+    textAlign: 'center',
+    fontFamily: 'Poppins-Regular',
+    marginTop:'5%'
+  },
   input:{ marginTop: 20,},
   inputLabel: {
-    fontSzie: 17,
-    fontWeight: 600,
-    color: '#222',
-    
     marginBottom: 8
   },
   inputControl: {
@@ -86,5 +111,28 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     color: '#222',
     marginTop: 10
+  },
+  form: {
+    marginBottom: 24,
+    flex: 1
+  },
+  formAction: {
+    marginVertical: 24,
+  },
+  logBtn: {
+    backgroundColor: '#38b6ff',
+    borderRadius: 20,
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '10%',
+    width: "50%",
+    marginHorizontal: '20%'
+  },
+  logBtnText:{
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#fff'
   }
 })
