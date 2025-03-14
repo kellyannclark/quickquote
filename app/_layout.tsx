3e472115fb95c0447c0afced26cafe2d298e390f
Shaira <sil21019@byui.edu>
2025-03-14 13:02:52 +0000

    page header titles updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ title: "Sign Up"}} />
+        <Stack.Screen name="dashboard" options={{ title: "Dashboard"}} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
