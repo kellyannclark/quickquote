5f1759e0aa53208cd14f45e8ec914a6b73c52ff4
Shaira <sil21019@byui.edu>
2025-03-14 15:58:05 +0000

    login tab symbol change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,7 +44,7 @@ export default function TabLayout() {
         name="login"
         options={{
           title: 'Log In',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="key.2.on.ring" color={color} />,
         }}
       />
     </Tabs>
