f4da47ff9c065e13d87978314c8ecbf3da2202d5
enochbalbuena <enxch@icloud.com>
2025-03-26 22:05:13 -0600

    image upload feature

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
@@ -20,3 +21,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export default app;
+export const storage = getStorage(app);
